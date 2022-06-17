cfb2762095f896534c3105eea1c95a28133b1bc6
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-06-17 13:46:37 -0300

    clientSideMessage

--- server.py
+++ server.py
@@ -12,29 +12,30 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 def handle_client(conn, addr):
-    print("[NEW CONNECTION] {addr} connected.")
+    print(f"[NEW CONNECTION] {addr} connected.")
 
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT) # How many bites we are going to send at a time.
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        if msg==DISCONNECT_MESSAGE:
-            connected = False
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
 
-        print("[{%}] {%}".format(addr, msg))
+            print(f"[{addr}] {msg}")
     conn.close()
 
 
 def start():
     server.listen()
-    print(f"[*] Listening on {SERVER}:{PORT}")
+    print(f"[LISTENING] Server is Listening on {SERVER}")
     while True:
         conn, addr = server.accept()
         print(f"[+] {addr} connected.")
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print("[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 print("[STARTING] server is starting...")
 start()
